1c9798de41c128878c7e5184d6cf030f9889a83f
prateek-sibit <prateeksoni2301@gmail.com>
2018-05-22 16:23:28 +0530

    added forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.views.generic import (View,TemplateView,DetailView,ListView,CreateView,UpdateView,DeleteView)
+from blog.models import Post,Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from blog.forms import PostForm,CommentForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    # comment out get_queryset and see how the output changes
+    # get_queryset works like a SQL Query ->
+     # Select * from Post where date<=current_date order by published_date desc
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date'))
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def qet_queryset(self):
+        # SELECT * FROM POST WHERE published_date=NULL;
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
