7870791c6e5632b3b4cbd4e2f7040f22c768591a
prateek-sibit <prateeksoni2301@gmail.com>
2018-05-24 15:33:45 +0530

    modified templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -18,7 +18,7 @@ class PostListView(ListView):
     # get_queryset works like a SQL Query ->
      # Select * from Post where date<=current_date order by published_date desc
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -55,7 +55,7 @@ class DraftListView(LoginRequiredMixin,ListView):
 @login_required
 def post_publish(request,pk):
     post = get_object_or_404(Post,pk=pk)
-    post.publish
+    post.publish()
     return redirect('post_detail',pk=pk)
 
 
